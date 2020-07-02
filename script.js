0cf25ac58a5c7cbd0a5466eaa16c70142299ca0d
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-07-02 22:03:03 +0200

    add script.js basic functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+//alert("Let the games Begin!!");
+function nextQuestion() {
+    document.getElementById("question").innerHTML = 'How has invented HTML?';
+    document.getElementById("answer1").innerHTML = 'Robbie Williams';
+    document.getElementById("answer2").innerHTML = 'Lady Gaga';
+    document.getElementById("answer3").innerHTML = 'Tim Berners-Lee';
+    document.getElementById("answer4").innerHTML = 'Justin Bieber';
+}
+
+function answer(a) {
+    //alert('Your Answer was' + a);
+    if(a == 'Tim Berners-Lee') {
+        alert('RIGHT!!');
+    } else {
+        alert('WRONG!');
+    }
+}
