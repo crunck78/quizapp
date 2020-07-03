532d12494dbd3fe820c694f70da220858d5df55f
crunck78 <crunck78@googlemai.com>
2020-07-03 17:49:07 +0200

    save mobile work

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 //alert("Let the games Begin!!");
+
+var quizes = [];
+var questions = [ 'How has invented HTML?' ];
+var answers = [ 'Robbie Williams', 'Lady Gaga', 'Tim Berners-Lee', 'Justin Bieber' ];
+
+function init(n){
+	for( var i = 0; i < n; i++)
+	{
+		quizes.push( createQuiz() );
+	}
+}
+
+function createQuizList() {
+	var quize = document.createElement('div');
+	var question = document.createElement('div');
+	var answerContainer = document.createElement('div');
+	quize.classList.add('card');
+	question.classList.add('card-body');
+	answerContainer.classList.add('mt-4');
+}
+
 function nextQuestion() {
     document.getElementById("question").innerHTML = 'How has invented HTML?';
     document.getElementById("answer1").innerHTML = 'Robbie Williams';
