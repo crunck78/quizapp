90736639ccb6b8a3d17f6886046e544e8a2d758e
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-07-03 23:55:08 +0200

    save

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 //alert('LET THE GAME BEGIN');
 
+var answer_1 = 'Robbie Williams';
+var answer_2 = 'Lady Gaga';
+var answer_3 = 'Tim Berners-Lee';
+var answer_4 = 'Justin Bieber';
+
 function nextQuestion() {
     document.getElementById("question").innerHTML = 'How has invented HTML?';
-    document.getElementById("answer1").innerHTML = 'Robbie Williams';
-    document.getElementById("answer2").innerHTML = 'Lady Gaga';
-    document.getElementById("answer3").innerHTML = 'Tim Berners-Lee';
-    document.getElementById("answer4").innerHTML = 'Justin Bieber';
+    document.getElementById("answer1").innerHTML = answer_1;
+    document.getElementById("answer2").innerHTML = answer_2;
+    document.getElementById("answer3").innerHTML = answer_3;
+    document.getElementById("answer4").innerHTML = answer_4;
 }
 
 function answer(a) {
-    //alert('Your Answer was' + a);
-    if(a == 'Tim Berners-Lee') {
-        alert('RIGHT!!');
+    
+    var selected_answer;
+    if(a == 1) {
+        selected_answer = answer_1;
+    } else if (a == 2){
+        selected_answer = answer_2;
+    } else if (a == 3){
+        selected_answer = answer_3;
     } else {
-        alert('WRONG!');
+        selected_answer = answer_4;
+    }
+
+    //alert('You answerd ' + selected_answer );
+
+    if(selected_answer == answer_3) {
+        document.getElementById('wrong-answer').classList.add('d-none');
+        document.getElementById('right-answer').classList.remove('d-none');
+    } else {
+        document.getElementById('wrong-answer').classList.remove('d-none');
+        document.getElementById('right-answer').classList.add('d-none');
     }
 }
