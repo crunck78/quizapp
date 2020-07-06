fb42ca8a3e3f3b28ec66ec0fc89afe22f043916e
crunck78 <crunck78@googlemai.com>
2020-07-06 03:57:16 +0200

    last changes

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 //alert('LET THE GAME BEGIN');
-var question_1 = 'How has invented HTML?';
-var question_1_answer_1 = 'Robbie Williams';
-var question_1_answer_2 = 'Lady Gaga';
-var question_1_answer_3 = 'Tim Berners-Lee';
-var question_1_answer_4 = 'Justin Bieber';
 
-
-var question_2 = 'What stands for HTML Tag &lta&gt?'
-var question_2_answer_1 = 'Text Bold';
-var question_2_answer_2 = 'Container';
-var question_2_answer_3 = 'Link';
-var question_2_answer_4 = 'Cursive';
-
-var question_3 = 'How to bind a Website inside a Website?'
-var question_3_answer_1 = '&lt;iframe&gt;, &lt;frame&gt; and &lt;frameset&gt;';
-var question_3_answer_2 = '&lt;iframe&gt;';
-var question_3_answer_3 = '&lt;frame&gt;';
-var question_3_answer_4 = '&lt;frameset&gt;';
+var allQuestions = [
+	{
+		'question' : 'How has invented HTML?',
+		'answer_1' : 'Robbie Williams',
+		'answer_2' : 'Lady Gaga',
+		'answer_3' : 'Tim Berners-Lee',
+		'answer_4' : 'Justin Bieber',
+		'right_answer' : 3
+	},
+	{
+		'question' : 'What stands for HTML Tag &lta&gt?',
+		'answer_1' : 'Text Bold',
+		'answer_2' : 'Container',
+		'answer_3' : 'Link',
+		'answer_4' : 'Cursive',
+		'right_answer' : 3
+	},
+	{
+		'question' : 'How to bind a Website inside a Website?',
+		'answer_1' : '&lt;iframe&gt;, &lt;frame&gt; and &lt;frameset&gt;',
+		'answer_2' : '&lt;iframe&gt;',
+		'answer_3' : '&lt;frame&gt;',
+		'answer_4' : '&lt;frameset&gt;',
+		'right_answer' : 2
+	}
+];
 
 var right_answer = 0;
 var question_number = 0;
@@ -28,51 +37,33 @@ function hideElements() {
 }
 
 function nextQuestion() {
-    
     hideElements();
-    
-    question_number = question_number + 1;
-    progress = question_number * 10;
-    document.getElementById('progress-bar').innerHTML = progress + '%';
-    document.getElementById('progress-bar').style.width = progress + '%';
-
-    if(question_number == 1) {
-       loadQuestion1();
-    }
-    if(question_number == 2) {
-        loadQuestion2();
-    }
-
-    if(question_number == 3) {
-        loadQuestion3();
-    }
+	
+	if(question_number == allQuestions.length) {
+		finishQuiz();
+	}
+	else {
+		question_number = question_number + 1;
+		progress = Math.round(question_number / allQuestions.length * 100);
+    	document.getElementById('progress-bar').innerHTML = progress + '%';
+    	document.getElementById('progress-bar').style.width = progress + '%';
+   		loadQuestion();
+	}
 }
 
-function loadQuestion1() {
-    document.getElementById("question").innerHTML = question_1;
-    document.getElementById("answer1").innerHTML = question_1_answer_1;
-    document.getElementById("answer2").innerHTML = question_1_answer_2;
-    document.getElementById("answer3").innerHTML = question_1_answer_3;
-    document.getElementById("answer4").innerHTML = question_1_answer_4;
-    right_answer = 3;
+function finishQuiz() {
+	//alert('Quiz Finished.');
+	document.getElementById('quiz-container').classList.add('d-none');
+	document.getElementById('quiz-finished-container').classList.remove('d-none');
 }
 
-function loadQuestion2() {
-    document.getElementById("question").innerHTML = question_2;
-    document.getElementById("answer1").innerHTML = question_2_answer_1;
-    document.getElementById("answer2").innerHTML = question_2_answer_2;
-    document.getElementById("answer3").innerHTML = question_2_answer_3;
-    document.getElementById("answer4").innerHTML = question_2_answer_4;
-    right_answer = 3;
-}
-
-function loadQuestion3() {
-    document.getElementById("question").innerHTML = question_3;
-    document.getElementById("answer1").innerHTML = question_3_answer_1;
-    document.getElementById("answer2").innerHTML = question_3_answer_2;
-    document.getElementById("answer3").innerHTML = question_3_answer_3;
-    document.getElementById("answer4").innerHTML = question_3_answer_4;
-    right_answer = 2;
+function loadQuestion() {
+    document.getElementById("question").innerHTML = allQuestions[question_number - 1]['question'];
+    document.getElementById("answer1").innerHTML = allQuestions[question_number - 1]['answer_1'];;
+    document.getElementById("answer2").innerHTML = allQuestions[question_number - 1]['answer_2'];;
+    document.getElementById("answer3").innerHTML = allQuestions[question_number - 1]['answer_3'];;
+    document.getElementById("answer4").innerHTML = allQuestions[question_number - 1]['answer_4'];;
+    right_answer = allQuestions[question_number - 1]['right_answer'];
 }
 
 function answer(a) {
